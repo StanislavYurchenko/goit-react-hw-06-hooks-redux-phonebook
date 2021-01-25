105a9589335c22a18a8f4bee6b150907ebd8e60f
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-01-25 16:24:15 +0200

    ready

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ import App from './components/App/App';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor} />
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
